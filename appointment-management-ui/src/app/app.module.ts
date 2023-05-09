78cae3ba9567cc6a7593a674f995733d1ca70154
kritikasinghsengar6421 <kritikasinghsengar6421@gmail.com>
2023-05-09 18:03:32 +0530

    added declaration for header and footer

--- appointment-management-ui/src/app/app.module.ts
+++ appointment-management-ui/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/components/header/header.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { SideBarComponent } from './shared/components/side-bar/side-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent,
+    SideBarComponent
   ],
   imports: [
     BrowserModule,
